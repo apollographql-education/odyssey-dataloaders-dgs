482d9e561e0d066fe2d493372f7d237ad0237c5f
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-03-26 12:27:38 -0600

    add log statement

--- src/main/java/com/example/soundtracks/datasources/SpotifyClient.java
+++ src/main/java/com/example/soundtracks/datasources/SpotifyClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.client.RestClient;
 
 @Component
 public class SpotifyClient {
-    private static final String SPOTIFY_API_URL = "https://spotify-demo-api-fe224840a08c.herokuapp.com/v1";
+    private static final String SPOTIFY_API_URL = "http://localhost:5001/v1";
     private final RestClient client = RestClient.builder().baseUrl(SPOTIFY_API_URL).build();
 
     public PlaylistCollection featuredPlaylistsRequest() {
