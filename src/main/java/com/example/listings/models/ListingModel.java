776928cc97dcf38e42b57280b0755723bc59bbd0
Liz Hennessy <95302380+lizhennessy@users.noreply.github.com>
2024-05-30 20:49:47 -0600

    Update ListingModel.java

--- src/main/java/com/example/listings/models/ListingModel.java
+++ src/main/java/com/example/listings/models/ListingModel.java
@@ -10,25 +10,4 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ListingModel extends Listing {
-
-    public ListingModel(String id, String title, String description, Integer numOfBeds, Double costPerNight, Boolean closedForBookings, List<Amenity> amenities) {
-        super(id, title,description, numOfBeds, costPerNight, closedForBookings, amenities);
-        this.setHash();
-    }
-
-    @Override
-    public String toString() {
-        return "Listing{" + "id='" + this.getId() + "'," +"title='" + this.getTitle() + "'," + "hash='" + this.getHash() + "'," +"description='" + this.getDescription() + "'," +"numOfBeds='" + this.getNumOfBeds() + "'," +"costPerNight='" + this.getCostPerNight() + "'," +"closedForBookings='" + this.getClosedForBookings() + "'," +"amenities='" + this.getAmenities() + "'" +"}";
-    }
-    public String hash;
-
-    public String getHash() {
-        return this.hash;
-    }
-
-    public void setHash() {
-        String titleAndId = this.getTitle() + this.getId();
-        System.out.println("titleAndId " + this.getTitle());
-        this.hash = titleAndId.replaceAll("\\s+", "");
-    }
 }
